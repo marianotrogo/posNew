cd94dccb09f593373c578979141e247a94d574b8
marianotrogo <marianoemmanuel_22@hotmail.com>
2025-04-13 09:11:04 -0300

    sidebard end 1part

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {FaHome,FaShoppingCart,FaBox, FaSignInAlt ,FaUserAlt, FaCog, FaBars} from 'react-icons/fa'
-import {IoCloseSharp} from 'react-icons/io'
+import {IoCloseSharp} from 'react-icons/io5'
 import {Link} from 'react-router-dom'
 
 const Sidebar = () => {
@@ -10,9 +10,11 @@ const Sidebar = () => {
 
   return (
     <div className="flex">
-        <div className="w-20 md:w-64 bg-gray-800 transition-width duration-300 text-white">
+        <div className={`fixed top-0 left-0 h-full md:w-64 bg-gray-800 transition-width duration-300 text-white
+            ${isOpen ? "w-64" : "w-20"}
+            `}>
             <div className="flex justify-between items-center p-4">
-                <h2 className={`text-xl font-bold hidden md:block ${isOpen ? "block" : "hidden"}`}>POS</h2>
+                <h2 className={`text-xl font-bold  md:block ${isOpen ? "block" : "hidden"}`}>POS</h2>
                 <button className='block md:hidden' onClick={()=> setIsOpen(!isOpen)}>
                    {isOpen ? <IoCloseSharp size={18} /> : <FaBars size={18}/> } 
                 </button>
@@ -22,26 +24,26 @@ const Sidebar = () => {
                 <ul>
                     <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                         <FaHome size={18}/>
-                        <span className='ml-4 hidden md:block'>Home</span>
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>Home</span>
                     </li>
                     <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                         <FaShoppingCart size={18}/>
-                        <span className='ml-4 hidden md:block'>Ventas</span>
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>Ventas</span>
                     </li>
                     <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                         <FaBox size={18}/>
-                        <span className='ml-4  hidden md:block'>Productos</span>
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>Productos</span>
                     </li>
                     <li className="flex  items-center p-4 hover:bg-gray-700 cursor-pointer">
                         <FaSignInAlt size={18}/>
-                        <span className='ml-4 hidden md:block'>Login</span>
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>Login</span>
                     </li>
 
                 </ul>
             </nav>
         </div>
         {/* Dashboard */}
-        <div className='p-8 bg-gray-100 min-h-screen flex-1'>
+        <div className='ml-20 md:ml-64 p-8 bg-gray-100 min-h-screen flex-1'>
             <h2 className='text-2xl font-bold'>Dashboard</h2>
             <p className=''>Dashboard to the right side</p>
 
